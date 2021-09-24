066241c6c86d8321b03152b721fefe2e163c7fad
Charles Renaud <renaudcharlespro@gmail.com>
2021-09-24 15:21:44 +0200

    profile v1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useLayoutEffect } from "react";
 import ReactFullpage from '@fullpage/react-fullpage';
 import "./App.css"
 import { CircularProgressbar } from 'react-circular-progressbar';
@@ -15,73 +15,83 @@ import react from "./images/react.png";
 import serveur from "./images/serveur2.png";
 import webpack from "./images/webpack4.png";
 import wordpress from "./images/wordpress2.png";
-import php from "./images/php.png"
+import php from "./images/php.png";
+import node from "./images/nodejs2.png"
+import certif from './images/certif.png'
+import moi from './images/moi.png';
+import logo from "./images/logo2.png";
+import diplome1 from "./images/diplome4.png";
+import diplome2 from "./images/diplome2.png";
+import diplome3 from "./images/diplome3.png";
+
 
 
 const slide = [
   [
     {
     "name" : "Javascript",
-    "value" : 80,
+    "value" : 70,
     'image' : javascript
     },
     {
     "name" : "React",
-    "value" : 70,
+    "value" : 60,
     'image' : react
     },
     {
     "name" : "Webpack",
-    "value" : 70,
+    "value" : 60,
     'image' : webpack
 
     },
     {
-      "name" : "MongoDB (noSQL)",
-      "value" : 70,
-      'image' : mongodb
-
-      },
+      "name" : "NodeJS",
+      "value" : 55,
+      'image' : node
+    },
   ],
-
 
   [
     {
-      "name" : "PHP",
+      "name" : "MongoDB (noSQL)",
       "value" : 60,
+      'image' : mongodb
+
+      },
+    {
+      "name" : "Wordpress",
+      "value" : 55,
+      'image' : wordpress
+    },
+    {
+      "name" : "PHP",
+      "value" : 55,
       'image' : php
 
     },
     {
-      "name" : "Wordpress",
-      "value" : 60,
-      'image' : wordpress
-
-    },
-    {
       "name" : "MySql",
-      "value" : 60,
+      "value" : 50,
       'image' : mysql
-
-    }
+    },
   ],
 
   [
     {
       "name" : "Linux",
-      "value" : 70,
+      "value" : 65,
       'image' : linux
 
     },
     {
       "name" : "Git",
-      "value" : 70,
+      "value" : 60,
       'image' : git
 
     },
     {
       "name" : "Méthodologie Agile",
-      "value" : 80,
+      "value" : 60,
       'image' : methodo
 
     },
@@ -94,44 +104,93 @@ const slide = [
 
 ];
 
-const TextAppear = () => {
-  const [placeholder, setPlaceholder] = React.useState('');
 
-  const
-    strings = [
-      "Frontend Javascript"
-    ],
-    index = React.useRef(0);
 
-  React.useEffect(() => {
-    function tick() {
-      setPlaceholder(prev => prev + strings[index.current]);
-      index.current++;
-    }
-    if (index.current < strings.length) {
-      let addChar = setInterval(tick, 50);
-      return () => clearInterval(addChar);
-    }
-  }, [placeholder]);
 
-  return (
-    <div className="text-display">
-      {placeholder}
+// ProfilPage /////////////////////////////////////////////////
+
+const ProfilPage = () => {
+
+
+  return(
+    <div className="profil-container">
+      <div className="profil-card">
+        <div className="box-image" style={{backgroundImage: `url(${moi})`}}></div>
+        <div className="card-list">
+          <p>- Charles Renaud</p>
+          <p>- 23 ans</p>
+          <p>- renaudcharlespro@gmail.com</p>
+          <p>- Développeur de WebApp </p>
+          <p>- Diplômer en Communication</p>
+          <p><a href="#">- Code du site </a></p>
+          <p><a href="#">- Programme de l'alternance  </a></p>
+
+        </div>
+        
+      </div>
+      <div className="description-box">
+        <div className="description line-1 anim-typewriters3"> Actuellement en recherche d'une alternance, afin de réaliser un bac+3 'Développeur / Concepteur d'appllications'</div>
+      </div>
+      <div className="diplome-box">
+      <div className="diplome-card" ><p className="date">- 2019 -</p><img className="diplome" src={diplome1} /><p>BTS - Communication (BAC+2)</p><a className="study-link" href="#">L'isme</a></div>
+      <div className="diplome-card" ><p className="date">- 2020 -</p> <img  className="diplome"src={diplome2} /><p>Formation de web développeur (BAC+2)</p><a className="study-link" href="#">Ecole O'clock</a></div>
+      <div className="diplome-card" ><p className="date">Début : 16 octobre 2021</p><img className="diplome" src={diplome3} /><p>Développeur, Concepteur d'apllications (BAC+3) </p><a className="study-link" href="#">Ecole O'clock</a></div>
+        
+      </div>
     </div>
   )
 }
 
 
+// Slider /////////////////////////////////////////////////
+
+
+
+const test = (slider) => {
+
+  if(slider[0].name === "Javascript"){
+    return 'Stack Javascript - '
+  }
+  else if(slider[0].name === "MongoDB (noSQL)"){
+    return 'Stack PHP & BDD - '
+  }
+  else{
+    return 'Administration Système - '
+}}
+
+
 const Slidergenerator = () => {
+
+  let prev = (document.getElementsByClassName("fp-controlArrow")[0]);
+  let next = (document.getElementsByClassName("fp-controlArrow")[1]);
+
+  const [toogler, setToogler] = useState("anim-typewriter")
+
+  const Toogling = () => {
+    if( prev && next !== undefined) {
+      if(toogler === "anim-typewriter" ){
+        prev.onclick = () => setToogler("anim-typewriters")
+        next.onclick = () => setToogler("anim-typewriters")
+      }
+      if(toogler === "anim-typewriters"){
+        prev.onclick = () => setToogler("anim-typewriter")
+        next.onclick = () => setToogler("anim-typewriter")
+      }
+    }
+  }
+   Toogling()
+
     return (
       <div>
         {slide.map(slider =>  
           <div className="slide">
             <div className="skill-container">
-                <TextAppear />
+              <h1 className={"line-1 " + toogler }>{test(slider)}</h1>
               <div className="skill-box" >
                 {slider.map(stat =>
                   <div className="skill-card" >
+                                    <img className="certif" src={certif} />
+
                     <VisibilitySensor>
                       {({ isVisible }) => {
                       const percentage = isVisible ? stat.value : 0;
@@ -155,30 +214,35 @@ const Slidergenerator = () => {
     )
   }
 
+ const Header = () => {
+   return(
+     <div className="header">
+       <img className="logo" src={logo} />
+     </div>
+
+   )
+ } 
 
 
 
-console.log(slide)
+const Fullpage = () => {
 
 
-
-const Fullpage = () => (
-
+return (  
+  <div>
+    <Header />
     <ReactFullpage
       //fullpage options
       scrollOverflow={true}
       scrollingSpeed = {1000} /* Options here */
-      sectionsColor={["lightgrey", "grey", "lightgrey"]}
+      sectionsColor={["#E2C2B3", "#687477",'#E2C2B3']}
       render={({ state, fullpageApi }) => {
 
         return (
         
-        <ReactFullpage.Wrapper>
+        <ReactFullpage.Wrapper >
           <div className="section">
-              <p>Charles Renaud</p>
-              <button onClick={() => fullpageApi.moveSectionDown()}>
-               - Parcours -
-              </button>
+              <ProfilPage />
           </div>
 
           <div className="section">
@@ -186,10 +250,12 @@ const Fullpage = () => (
           </div>
 
           <div className="section"></div>
+
         </ReactFullpage.Wrapper>
         );
       }}
     />
-  );
+  </div>
+)};
 
   export default Fullpage;
